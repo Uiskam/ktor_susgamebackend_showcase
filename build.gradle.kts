39dad950b014ad9bae700333a00b3be1a6125870
Uiskam <maksulima@gmail.com>
2024-05-17 21:37:19 +0200

    rest of the files

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+
+val ktor_version: String by project
+val kotlin_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("io.ktor.plugin") version "2.3.11"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
+kotlin {
+    jvmToolchain(21)
+}
